33bb6f3d5fb47619cfb032dfecc74e293dbcb62c
RohanKumarSingh24 <singh.rohan72@gmail.com>
2019-07-06 16:58:16 +0530

    rename function name

--- stop.py
+++ stop.py
@@ -2,15 +2,15 @@ import os
 import re
 import glob
 
-path="Inputfile"
-path1="outputfile"
+path="Inputfile"        # Give  input file path here
+path1="outputfile"      # Give  path for output foldres where you want to save the results
 
-def Remove_Stop_Word(stopwords):
+def remove_stop_word(stopwords):
 	"""
 	"""
-	for Inpfile in glob.glob(os.path.join(path,'*')):
-		output_file_name=Inpfile.rsplit("\\",1)[1]
-		review_file=open(Inpfile,'r').read().split()
+	for inpfile in glob.glob(os.path.join(path,'*')):
+		output_file_name=inpfile.rsplit("\\",1)[1]
+		review_file=open(inpfile,'r').read().split()
 		filter_file=[]
 		for word in review_file:
 			if re.sub('[^[A-Za-z0-9]+'," ",word) not in stopwords:
@@ -21,6 +21,6 @@ def Remove_Stop_Word(stopwords):
 
 if __name__ == '__main__':
 	with open("stopwords.txt","r") as stopword_file:
-		Remove_Stop_Word(list(map(lambda x: x.strip("\n").lower(),stopword_file.readline())))
+		remove_stop_word(list(map(lambda x: x.strip("\n").lower(),stopword_file.readline())))
 								
 
